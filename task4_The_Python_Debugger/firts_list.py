131802d25967bb3c2a317f0e7297f837ec28b5e0
b0-0sk <arnaubosch2dam@gmail.com>
2019-10-22 19:15:43 +0200

    Tuesday 19:15 22/10/2019

--- task4_The_Python_Debugger/firts_list.py
+++ task4_The_Python_Debugger/firts_list.py
@@ -4,9 +4,9 @@ class firts_list:
     El programa et retornar els numeros primers que desitgis.Si el usuari introduiex com a argument un 4 
     el programa et retornara els 4 primers numeros primaris.
     
-    >>> python firts_list.py 4
+    >>> firts_list(4)
     [2, 3, 5, 7]
-    >>> python firts_list.py 65
+    >>> firts_list(65)
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
     103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
     211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313]
