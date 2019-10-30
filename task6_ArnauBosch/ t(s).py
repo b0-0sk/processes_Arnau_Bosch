c9315a7ae5dfceb8d25e9d5bd36335a90c6e8b5d
b0-0sk <arnaubosch2dam@gmail.com>
2019-10-30 19:39:03 +0100

    value add

--- task6_ArnauBosch/ t(s).py
+++ task6_ArnauBosch/ t(s).py
@@ -1,12 +1,16 @@
-from multiprocessing import Process
+from multiprocessing import Process, Value
 from datetime import datetime
 import time
+
+a=Value('i',3)
 
 def t(s):
     while(True):
        time.sleep(s)
        print(datetime.now())
-
+       print a.value
+       
+       
        
 def main():
     p = Process(target=t, args=(1,))
@@ -15,8 +19,9 @@ def main():
     for i in range(10):
         time.sleep(0.5)
         print(p.pid)
+        a.value=a.value+1
 
-    #p.terminate()
+    p.terminate()
     
     print("Hasta luego lucas")
 if __name__ == '__main__':
