7c7365420131159f6170a9f513b9cf30645b3319
Arnau Bosch Gomez <56040620+b0-0sk@users.noreply.github.com>
2019-12-03 18:53:11 +0100

    Add files via upload

---
+++ task11_ArnauBosch/task11.py
@@ -0,0 +1,57 @@
+# -*- coding: utf8 -*-
+import md5, random, sys
+from multiprocessing import Process, Semaphore
+
+def busca(x):
+    f = open('fitxer.txt', 'r')
+    fr = f.read()
+    index = fr.find('\n'+x+',')
+    index2 = fr.find('\n', index+1)
+    print fr[index+1:index2]
+    f.close()
+
+def substitueix(x):
+
+    lock.acquire()
+
+    f = open('fitxer.txt', 'r')
+    fr = f.read()
+    f.close()
+    index = fr.find('\n'+x+',')
+    index2 = fr.find('\n', index+1)
+    if index == -1:
+        return 0
+    print fr[index+1:index2]
+    f = open('fitxer.txt', 'w')
+    f.write(fr[:index+1])
+    f.write(str(100)+ ',' + md5.md5(str(100)).hexdigest()+ "\n")
+    f.write(fr[index2+1:])
+    f.close()
+    busca('100')
+
+    lock.release()
+
+def inici():
+
+    f = open('fitxer.txt', 'w')
+    for i in range(100):
+        f.write(str(i)+ ',' + md5.md5(str(i)).hexdigest()+ "\n")
+    f.close()
+    #print open('fitxer.txt', 'ro').read()
+
+if __name__ == '__main__' :
+
+    inici()
+    lock = Semaphore(1)
+
+    p4 = Process(target=substitueix, args=["4",])
+    p10 = Process(target=substitueix, args=["10",])
+    p60 = Process(target=substitueix, args=("60",))
+
+    p4.start()
+    p10.start()
+    p60.start()
+
+    p4.join()
+    p10.join()
+    p60.join()
